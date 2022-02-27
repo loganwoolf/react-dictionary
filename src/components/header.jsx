0c9042c2ca0008c0ffd38c5869b814c96ba226f9
Logan Woolf <64603789+loganwoolf@users.noreply.github.com>
2022-02-27 10:49:26 -0700

    initial commit

---
+++ src/components/header.jsx
@@ -0,0 +1,8 @@
+export default function Header(props) {
+  return (
+    <header>
+      <h1>World's Fastest React Dictionary</h1>
+      <h2>Definitions before you can even think!</h2>
+    </header>
+  )
+}
