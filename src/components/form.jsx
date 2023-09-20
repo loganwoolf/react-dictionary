9e4afe67218ee5a75964733f6ccc128a9e56b8ba
Logan Woolf <loganwoolf@gmail.com>
2023-09-20 04:22:44 +0000

    Focus input after using clear button

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,17 +1,27 @@
+import { useRef } from 'react'
+
 export default function Form(props) {
   const { term, setTerm, setDefinition } = props
+
+  const inputElement = useRef()
+
+  const focusInput = () => inputElement.current.focus()
 
   return (
     <>
       <input
+        ref={inputElement}
         placeholder="Get a definition"
+        name="define"
         value={term}
         onChange={(e) => setTerm(e.target.value)}
       />
       <button
+        type="button"
         onClick={() => {
           setTerm('')
           setDefinition('')
+          focusInput()
         }}
       >
         Clear
